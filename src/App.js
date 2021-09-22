5e5a19b823f0c7ce3881122a6723328cd8137499
kkrista <Kristamari.katajisto@gmail.com>
2021-09-22 19:26:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather defaultCity="New York" />
 
         <footer>
           This project was coded by{" "}
