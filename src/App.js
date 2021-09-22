cf47b2e4bd814c01b6509478eaeb788af627e149
kkrista <Kristamari.katajisto@gmail.com>
2021-09-22 18:32:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
 
-export default App;
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Krista-Mari Katajisto
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://youthful-bhaskara-9f96c7.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
